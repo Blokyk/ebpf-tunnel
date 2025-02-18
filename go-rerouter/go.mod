10e5f6e3b43b4c07186329c61ee62ad6a81044da
blokyk <courvoisier.zoe@gmail.com>
2025-02-18 17:23:48 +0100

    wip(rerouter): pin programs by default + start cli

--- go-rerouter/go.mod
+++ go-rerouter/go.mod
@@ -9,9 +9,13 @@ require (
 
 require (
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/opencontainers/runtime-spec v1.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/urfave/cli/v2 v2.27.5 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
--- go-rerouter/rerouter.go
+++ go-rerouter/rerouter.go
@@ -4,9 +4,14 @@ package main
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 -type Config rerouter rerouter.ebpf.c -- -g -I/usr/include/i386-linux-gnu -I/usr/i686-linux-gnu/include
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"syscall"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
@@ -17,6 +22,7 @@ const (
 	CGROUP_PATH     = "/sys/fs/cgroup" // Root cgroup path
 	TUNNEL_PORT     = 18000            // Port where the tunnel (in go or c) listens
 	REAL_PROXY_PORT = 8080             // Port where the real proxy (cntlm) listens
+	BPF_FS          = "/sys/fs/bpf/rerouter"
 )
 
 type rerouterLinks struct {
@@ -35,6 +41,17 @@ func (links *rerouterLinks) Close() {
 	links.PostBind4Link.Close()
 	links.CloneProbe.Close()
 	links.Clone3Probe.Close()
+}
+
+func (links *rerouterLinks) Iterate() map[string]link.Link {
+	return map[string]link.Link{
+		"Connect4Link":  links.Connect4Link,
+		"SockOpsLink":   links.SockOpsLink,
+		"SockOptLink":   links.SockOptLink,
+		"PostBind4Link": links.PostBind4Link,
+		"CloneProbe":    links.CloneProbe,
+		"Clone3Probe":   links.Clone3Probe,
+	}
 }
 
 // Attach eBPF programs to the root cgroup and the right kprobes
@@ -95,25 +112,125 @@ func attachProgs(objs rerouterObjects) (rerouterLinks, error) {
 	}, nil
 }
 
-func main() {
-	// Remove resource limits for kernels <5.11.
-	if err := rlimit.RemoveMemlock(); err != nil {
-		log.Print("Removing memlock:", err)
+func getPinPath(objName string) string { return filepath.Join(BPF_FS, objName) }
+
+func pinProg(prog *ebpf.Program, progName string) error {
+	pinPath := getPinPath(progName)
+
+	err := prog.Pin(pinPath)
+	if err != nil {
+		return fmt.Errorf("failed to pin program '%s': %w", progName, err)
 	}
 
-	// Load the compiled eBPF ELF and load it into the kernel
-	// NOTE: we could also pin the eBPF program
-	var objs rerouterObjects
-	if err := loadRerouterObjects(&objs, nil); err != nil {
+	return nil
+}
+
+func loadPinned(progName string) (*ebpf.Program, error) {
+	pinPath := getPinPath(progName)
+	pinnedProg, err := ebpf.LoadPinnedProgram(pinPath, &ebpf.LoadPinOptions{})
+
+	if err != nil {
+		return nil, fmt.Errorf("couldn't load already-pinned program '%s': %w", progName, err)
+	}
+
+	return pinnedProg, nil
+}
+
+func loadAndPinProgs(col *ebpf.Collection, _ *ebpf.CollectionSpec) error {
+	for progName, prog := range col.Programs {
+		err := pinProg(prog, progName)
+
+		// program is already pinned
+		if errors.Is(err, syscall.EEXIST) {
+			prog, err = loadPinned(progName)
+			if err != nil {
+				return fmt.Errorf("failed to load '%s' from pin: %w", progName, err)
+			}
+			col.Programs[progName] = prog
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func pinLink(link link.Link, linkName string) error {
+	pinPath := getPinPath(linkName)
+
+	err := link.Pin(pinPath)
+	if err != nil {
+		return fmt.Errorf("failed to pin link '%s': %w", linkName, err)
+	}
+
+	return nil
+}
+
+func pinAllLinks(links *rerouterLinks) error {
+	for linkName, link := range links.Iterate() {
+		err := pinLink(link, linkName)
+
+		// todo: should be !errors.Is(err, syscall.ENOTSUP) (+EECIST) but ebpf-go uses %v instead of %w >:|
+		if err != nil && !strings.HasSuffix(err.Error(), "not supported") && !strings.HasSuffix(err.Error(), "file exists") {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func removeMemlock() {
+	// Remove resource limits for kernels <5.11.
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatalf("Couldn't remove ressource limit: %v. (hint: you need to run this as root)", err)
+	}
+}
+
+func mmain() {
+	rerouterSpec, err := loadRerouter()
+	if err != nil {
 		log.Fatalf("Error loading eBPF program: %v", err)
+	}
+
+	// make sure the /sys/fs/bpf/rerouter folder exists so we can our objects there
+	err = os.Mkdir(BPF_FS, 0755)
+	if err != nil && !errors.Is(err, fs.ErrExist) {
+		log.Fatalf("Failed to create rerouter bpf pin directory: %b", err)
+	}
+
+	// read the programs from binary into a collection
+	rerouterCol, err := ebpf.NewCollectionWithOptions(rerouterSpec, ebpf.CollectionOptions{Maps: ebpf.MapOptions{PinPath: BPF_FS}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rerouterCol.Close()
+
+	// pin the programs or load them from the pin
+	err = loadAndPinProgs(rerouterCol, rerouterSpec)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// write the col to a type-safe representation for attachprogs
+	// todo: rewrite this to use the collection directly, i have a stash at home
+	var objs rerouterObjects
+	if err := rerouterCol.Assign(&objs); err != nil {
+		log.Fatalf("Error assigning loaded eBPF program: %v. (hint: you probably forgot to do `go generate`.)", err)
 	}
 	defer objs.Close()
 
 	links, err := attachProgs(objs)
 	if err != nil {
-		log.Fatalf("Couldn't attach some programs: %v", err)
+		log.Fatalf("Couldn't attach some programs: %v. (hint: you probably forgot to do `go generate`.)", err)
 	}
 	defer links.Close()
+
+	err = pinAllLinks(&links)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Update the rerouter map with the proxy server configuration, because we need to know the proxy server PID in order
 	// to filter out eBPF events generated by the proxy server itself so it would not proxy its own packets in a loop.
